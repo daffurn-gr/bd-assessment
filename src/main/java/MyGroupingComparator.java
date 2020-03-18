9f6d045af7dacd8fc2e6f23f319d1591af8d933c
daffurn-gr <0801676d@student.gla.ac.uk>
2020-03-18 07:02:37 +0000

    Added grouping comparator.

--- src/main/java/MyGroupingComparator.java
+++ src/main/java/MyGroupingComparator.java
@@ -1,6 +1,21 @@
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
+
+/* Group Pairs by the natural key.
+ * Source: http://codingjunkie.net/secondary-sort/.
+ */
 public class MyGroupingComparator extends WritableComparator {
 	
+	public MyGroupingComparator() {
+		super(Pair.class);
+	}
 
+	@Override
+	public int compare(WritableComparable a, WritableComparable b) {
+		// TODO Auto-generated method stub
+		Pair first  = (Pair) a;
+		Pair second = (Pair) b;
+		return first.getTerm().compareTo(second.getTerm());
+	}
 }
